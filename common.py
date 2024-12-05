fada10a06ae18845072779e6b15f464947027acc
waab76 <waab@me.com>
2024-12-05 08:45:10 -0500

    Day 5 - Print Queue

--- common.py
+++ common.py
@@ -1,3 +1,11 @@
 def get_input_lines(filename:str) -> list:
     input = open(filename, 'r')
     return [line.strip() for line in input.readlines()]
+
+def grid_get(grid:list, col: int, row: int, default:str='.') -> str:
+    if row < 0 or col < 0:
+        return default
+    try:
+        return grid[row][col]
+    except IndexError:
+        return default
