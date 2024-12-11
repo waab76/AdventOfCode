6164e044d107faad10b4f7f243dddc86e554d686
waab76 <waab@me.com>
2024-12-10 20:23:49 -0500

    2016 Day 16

---
+++ 2015/day16-1.py
@@ -0,0 +1,31 @@
+from common import get_input_lines
+import re
+
+def main():
+    input = get_input_lines('day16.txt')
+    right_sue = {
+        'children': 3,
+        'cats': 7,
+        'samoyeds': 2,
+        'pomeranians': 3,
+        'akitas': 0,
+        'vizslas': 0,
+        'goldfish': 5,
+        'trees': 3,
+        'cars': 2,
+        'perfumes': 1
+    }
+
+    for line in input:
+        sue_match = True
+        for attribute in right_sue.keys():
+            check = attribute + ': (\d+)'
+            match = re.search(check, line)
+            if match:
+                sue_match &= right_sue[attribute] == int(match.group(1))
+        if sue_match:
+            print(line)
+            quit()
+
+if __name__ == '__main__':
+    main()
