d8c8b66f629d8fbcd1a00b6a3cb6b73fa2be1069
Rob Curtis <rob.curtis@graylog.com>
2025-12-11 17:17:09 -0500

    2025 Day 11

--- 2025/aoc_11.py
+++ 2025/aoc_11.py
@@ -1,22 +1,35 @@
+from copy import copy
+from functools import cache
 from common import get_input_lines
 
+nodes = {}
+
 def _parse_input(lines:list[str]) -> map:
-    nodes = {}
     for line in lines:
         key = line[:line.index(':')]
         vals = line[line.index(':') + 1:].split()
         nodes[key] = vals
-    return nodes
+
+@cache
+def _count_paths(start: str, seen_fft: bool, seen_dac: bool) -> int:
+    if start == 'out':
+        if seen_fft and seen_dac:
+            return 1
+        return 0
+    my_seen_fft = seen_fft or start == 'fft'
+    my_seen_dac = seen_dac or start == 'dac'
+    return sum(_count_paths(next, my_seen_fft, my_seen_dac) for next in nodes[start])
 
 def _part1():
-    pass
-
+    paths = _count_paths('you', True, True)
+    print(f'Paths to out: {paths}')
+    
 def _part2():
-    pass
+    paths = _count_paths('svr', False, False)
+    print(f'Paths from svr to out that include fft and dac: {paths}')
 
 def main():
-    data = get_input_lines('/Users/rcurtis/Workspace/AdventOfCode/2025/aoc_11.txt')
-    nodes = _parse_input(data)
+    _parse_input(get_input_lines('aoc_11.txt'))
 
     _part1()
     _part2()
